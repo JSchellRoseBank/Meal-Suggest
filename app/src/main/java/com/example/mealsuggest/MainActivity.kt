d7b488c6bcbc3411c66b34986e593333c2e1e8c0
JSchellRoseBank <ST10486426@rcconnect.edu.za>
2025-04-01 13:33:10 +0200

    Added main heading

--- app/src/main/java/com/example/mealsuggest/MainActivity.kt
+++ app/src/main/java/com/example/mealsuggest/MainActivity.kt
@@ -4,22 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mealsuggest.ui.theme.MealSuggestTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
+        setContent() {
             MealSuggestTheme {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxSize())
+                {
+                    Text(
+                        text = "Meal Suggest",
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Black
+                    )
 
+
+                }
             }
         }
     }
